857d07cb0872d0fe3ad35be617be2de174acd045
qafarlicavid <javidig@code.edu.az>
2023-03-08 20:08:23 +0400

    some configurations prepared for project

---
+++ JavidElectronics/JavidElectronics/Infrastructure/Configurations/OptionConfigurations.cs
@@ -0,0 +1,13 @@
+﻿namespace JavidElectronics.Infrastructure.Configurations
+{
+    public static class OptionConfigurations
+    {
+        public static void ConfigureOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            //var emailConfig = configuration.GetSection("EmailConfiguration").Get<EmailConfigOptions>();
+            //services.AddSingleton(emailConfig);
+
+            //services.Configure<EmailConfigOptions>(configuration.GetSection(nameof(EmailConfigOptions)));
+        }
+    }
+}
