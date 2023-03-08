870c0d8fa1a87716a319f039e76a11b5e27d9428
qafarlicavid <javidig@code.edu.az>
2023-03-08 23:40:53 +0400

    File added to Contracts

---
+++ JavidElectronics/JavidElectronics/Contracts/File/UploadDirectory.cs
@@ -0,0 +1,11 @@
+﻿namespace JavidElectronics.Contracts.File
+{
+    public enum UploadDirectory
+    {
+        Product = 1,
+        Slider = 2,
+        PaymentBenefit = 4,
+        BlogImage = 8,
+        BlogVideo = 16,
+    }
+}
