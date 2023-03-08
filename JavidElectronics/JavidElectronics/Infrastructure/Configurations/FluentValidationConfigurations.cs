f3b269dfe81ec088ddcb866f15d3f786b9afd429
qafarlicavid <javidig@code.edu.az>
2023-03-08 20:07:30 +0400

    Fluent Validation is available now

---
+++ JavidElectronics/JavidElectronics/Infrastructure/Configurations/FluentValidationConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+
+namespace JavidElectronics.Infrastructure.Configurations
+{
+    public static class FluentValidationConfigurations
+    {
+        public static void ConfigureFluentValidatios(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Program>();
+        }
+    }
+}
