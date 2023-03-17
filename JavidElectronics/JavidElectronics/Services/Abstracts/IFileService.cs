b5f2730400894be087875eee19d890d85655d87a
qafarlicavid <javidig@code.edu.az>
2023-03-17 18:50:14 +0400

    FileService created

---
+++ JavidElectronics/JavidElectronics/Services/Abstracts/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using JavidElectronics.Contracts.File;
+
+namespace JavidElectronics.Services.Abstracts
+{
+    public interface IFileService
+    {
+        Task<string> UploadAsync(IFormFile formFile, UploadDirectory uploadDirectory);
+        string GetFileUrl(string? fileName, UploadDirectory uploadDirectory);
+        Task DeleteAsync(string? fileName, UploadDirectory uploadDirectory);
+    }
+}
