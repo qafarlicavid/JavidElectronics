b394ac0e20d00d2f2bec2c6aca692bd201694c42
qafarlicavid <javidig@code.edu.az>
2023-03-17 19:47:59 +0400

    PaymentBenefit from Client's side is ready

---
+++ JavidElectronics/JavidElectronics/Areas/Client/ViewComponents/PaymentBenefit/Benefit.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JavidElectronics.Areas.Client.ViewModels.Benefit;
+using JavidElectronics.Contracts.File;
+using JavidElectronics.Database;
+using JavidElectronics.Services.Abstracts;
+
+namespace JavidElectronics.Areas.Client.ViewComponents.PaymentBenefit
+{
+    [ViewComponent(Name = "Benefit")]
+    public class Benefit : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+        private readonly IFileService _fileService;
+
+
+        public Benefit(DataContext dataContext, IFileService fileService)
+        {
+            _dataContext = dataContext;
+            _fileService = fileService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model =
+                _dataContext.PaymentBenefits.Select(b => new BenefitListItemViewModel(b.Name!, b.Content!, _fileService
+                 .GetFileUrl(b.ImageNameInFileSystem, UploadDirectory.PaymentBenefit))).ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ JavidElectronics/JavidElectronics/Areas/Client/ViewModels/Benefit/BenefitListItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace JavidElectronics.Areas.Client.ViewModels.Benefit
+{
+    public class BenefitListItemViewModel
+    {
+        public BenefitListItemViewModel(string name, string content, string imageUrl)
+        {
+            Name = name;
+            Content = content;
+            ImageUrl = imageUrl;
+        }
+
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
