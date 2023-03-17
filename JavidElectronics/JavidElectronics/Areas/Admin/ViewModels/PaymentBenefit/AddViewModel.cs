f77563fb8eb61015caadc6b2b6de2b7400782500
qafarlicavid <javidig@code.edu.az>
2023-03-17 19:47:29 +0400

    PaymentBenefit from Admin's side is ready

---
+++ JavidElectronics/JavidElectronics/Areas/Admin/ViewModels/PaymentBenefit/AddViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JavidElectronics.Areas.Admin.ViewModels.PaymentBenefit
+{
+    public class AddViewModel
+    {
+        public string? Name { get; set; }
+        public string? Content { get; set; }
+        public IFormFile? ImageName { get; set; }
+
+    }
+}
---
+++ JavidElectronics/JavidElectronics/Areas/Admin/ViewModels/PaymentBenefit/UpdateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JavidElectronics.Areas.Admin.ViewModels.PaymentBenefit
+{
+    public class UpdateViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Content { get; set; }
+        public IFormFile? ImageName { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
