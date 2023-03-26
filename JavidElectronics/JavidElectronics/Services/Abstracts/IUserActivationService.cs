efcc55ede39e3d702ca68b3d8b8896a6bd200fd3
qafarlicavid <javidig@code.edu.az>
2023-03-26 13:32:38 +0400

    UserActivationService is ready

---
+++ JavidElectronics/JavidElectronics/Services/Abstracts/IUserActivationService.cs
@@ -0,0 +1,9 @@
+﻿using JavidElectronics.Database.Models;
+
+namespace JavidElectronics.Services.Abstracts
+{
+    public interface IUserActivationService
+    {
+        Task SendActivationUrlAsync(User user);
+    }
+}
