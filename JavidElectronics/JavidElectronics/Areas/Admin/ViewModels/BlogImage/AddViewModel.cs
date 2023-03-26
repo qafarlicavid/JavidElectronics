5f75d1b8b7526519791561025ffbb3354286811f
qafarlicavid <javidig@code.edu.az>
2023-03-26 23:32:39 +0400

    Controlling BlogImage from Admin's side is ready

---
+++ JavidElectronics/JavidElectronics/Areas/Admin/ViewModels/BlogImage/AddViewModel.cs
@@ -0,0 +1,10 @@
+﻿using JavidElectronics.Database.Models.Enums;
+
+namespace JavidElectronics.Areas.Admin.ViewModels.BlogImage
+{
+    public class AddViewModel
+    {
+        public OrderStatus? Order { get; set; }
+        public IFormFile? Image { get; set; }
+    }
+}
---
+++ JavidElectronics/JavidElectronics/Areas/Admin/ViewModels/BlogImage/UpdateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using JavidElectronics.Database.Models.Enums;
+
+namespace JavidElectronics.Areas.Admin.ViewModels.BlogImage
+{
+    public class UpdateViewModel
+    {
+        public string? ImageUrL { get; set; }
+        public OrderStatus? Order { get; set; }
+        public IFormFile? Image { get; set; }
+    }
+}
