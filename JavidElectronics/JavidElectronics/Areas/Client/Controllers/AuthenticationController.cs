85a68f0bef70969b3c7455385ded453c4968ef4d
qafarlicavid <javidig@code.edu.az>
2023-04-03 21:41:27 +0400

    Authentication from Client's side is ready

---
+++ JavidElectronics/JavidElectronics/Areas/Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,124 @@
+﻿using JavidElectronics.Areas.Client.ViewModels.Authentication;
+using JavidElectronics.Database;
+using JavidElectronics.Database.Models;
+using JavidElectronics.Services.Abstracts;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Security.Claims;
+using JavidElectronics.Contracts.Identity;
+
+namespace JavidElectronics.Areas.Client.Controllers
+{
+    [Area("client")]
+    [Route("auth")]
+    public class AuthenticationController : Controller
+    {
+        private readonly DataContext _dbContext;
+        private readonly IUserService _userService;
+
+        public AuthenticationController(DataContext dbContext, IUserService userService)
+        {
+            _dbContext = dbContext;
+            _userService = userService;
+        }
+
+        #region Login and Logout
+
+        [HttpGet("login", Name = "client-auth-login")]
+        public async Task<IActionResult> LoginAsync()
+        {
+            if (_userService.IsAuthenticated)
+            {
+                return RedirectToRoute("client-account-dashboard");
+            }
+
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost("login", Name = "client-auth-login")]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            if (!await _userService.CheckPasswordAsync(model!.Email, model!.Password))
+            {
+                ModelState.AddModelError(String.Empty, "Email or password is not correct");
+                return View(model);
+            }
+            if (await _dbContext.Users.AnyAsync(u => u.Email == model.Email && u.Role.Name == RoleNames.ADMIN))
+            {
+                await _userService.SignInAsync(model!.Email, model!.Password, RoleNames.ADMIN);
+                return RedirectToRoute("admin-auth-login");
+            }
+
+            await _userService.SignInAsync(model!.Email, model!.Password);
+
+            return RedirectToRoute("client-home-index");
+        }
+
+        [HttpGet("logout", Name = "client-auth-logout")]
+        public async Task<IActionResult> LogoutAsync()
+        {
+            await _userService.SignOutAsync();
+
+            return RedirectToRoute("client-home-index");
+        }
+
+        #endregion
+
+        #region Register
+
+        [HttpGet("register", Name = "client-auth-register")]
+        public ViewResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost("register", Name = "client-auth-register")]
+        public async Task<IActionResult> RegisterAsync(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await _userService.CreateAsync(model);
+
+            return RedirectToRoute("client-auth-login");
+        }
+
+        [HttpGet("activate/{token}", Name = "client-auth-activate")]
+        public async Task<IActionResult> ActivateAsync([FromRoute] string token)
+        {
+            var userActivation = await _dbContext.UserActivations
+                .Include(ua => ua.User)
+                .FirstOrDefaultAsync(ua =>
+                    !ua!.User!.IsEmailConfirmed &&
+                    ua.ActivationToken == token);
+
+            if (userActivation is null)
+            {
+                return NotFound();
+            }
+
+            if (DateTime.Now > userActivation!.ExpireDate)
+            {
+                return Ok("Token expired olub :( ");
+            }
+
+            userActivation!.User!.IsEmailConfirmed = true;
+
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToRoute("client-auth-login");
+        }
+
+        #endregion
+    }
+
+}
