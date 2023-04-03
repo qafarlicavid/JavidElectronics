c42fc53263df12bcc657f5c35152d46bfc118912
qafarlicavid <javidig@code.edu.az>
2023-04-03 21:46:32 +0400

    Controlling Orders from Admin's side is ready

---
+++ JavidElectronics/JavidElectronics/Areas/Admin/ViewModels/Order/ListItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using JavidElectronics.Database.Models.Enums;
+
+namespace JavidElectronics.Areas.Admin.ViewModels.Order
+{
+    public class ListItemViewModel
+    {
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Total { get; set; }
+        public ListItemViewModel(string id, DateTime createdtAt, OrderStatus status, decimal total)
+        {
+            Id = id;
+            CreatedAt = createdtAt;
+            Status = status;
+            Total = total;
+        }
+
+    }
+}
---
+++ JavidElectronics/JavidElectronics/Areas/Admin/ViewModels/Order/UpdateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using JavidElectronics.Database.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace JavidElectronics.Areas.Admin.ViewModels.Order
+{
+    public class UpdateViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public OrderStatus Status { get; set; }
+    }
+}
